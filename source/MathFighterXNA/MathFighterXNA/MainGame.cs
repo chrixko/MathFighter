74a2fdc8518e5e490cf5bdb7d0c992d5ed5d661a
Christian Kokott <christian.kokott@online.de>
2013-03-12 20:22:14 +0100

    fixed formatting in MainGame.cs

--- source/MathFighterXNA/MathFighterXNA/MainGame.cs
+++ source/MathFighterXNA/MathFighterXNA/MainGame.cs
@@ -1,21 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Microsoft.Kinect;
-using System.Threading;
 using MathFighterXNA.Screens;
 
-namespace MathFighterXNA
-{
-    public class MainGame : Microsoft.Xna.Framework.Game
-    {
+namespace MathFighterXNA {
+
+    public class MainGame : Microsoft.Xna.Framework.Game {
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         KinectContext kinectContext;
@@ -38,8 +29,7 @@ namespace MathFighterXNA
 
         public GameScreen CurrentScreen;
 
-        public MainGame()
-        {
+        public MainGame() {
             graphics = new GraphicsDeviceManager(this); 
             Content.RootDirectory = "Content";
 
@@ -49,8 +39,7 @@ namespace MathFighterXNA
             this.viewPortRectangle = new Rectangle(0, 0, Width, Height);
         }
 
-        protected override void Initialize()
-        {
+        protected override void Initialize() {
             Assets.LoadContent(Content);
             kinectContext = new KinectContext(graphics.GraphicsDevice);
             kinectContext.Initialize();
@@ -63,21 +52,15 @@ namespace MathFighterXNA
             base.Initialize();
         }
 
-        protected override void LoadContent()
-        {           
+        protected override void LoadContent() {           
             spriteBatch = new SpriteBatch(GraphicsDevice);            
         }
 
-        protected override void UnloadContent()
-        {
+        protected override void UnloadContent() {
             kinectContext.Sensor.Stop();
         }
 
-        protected override void Update(GameTime gameTime)
-        {           
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
+        protected override void Update(GameTime gameTime) {           
             debugComponent.Update(gameTime);
             
             kinectContext.Update();
@@ -87,18 +70,17 @@ namespace MathFighterXNA
             base.Update(gameTime);
         }
 
-        protected override void Draw(GameTime gameTime)
-        {
+        protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.Black);
             
             spriteBatch.Begin();
 
+            //TODO move kinectContext into the corresponding screens
             if (kinectContext.CurrentBitmap != null) {
                 spriteBatch.Draw(kinectContext.CurrentBitmap, new Rectangle(KinectOffsetX, KinectOffsetY, KinectWidth, KinectHeight), Color.White);
             }
 
             CurrentScreen.Draw(spriteBatch);
-
 
             debugComponent.Draw(spriteBatch, gameTime);
             spriteBatch.End();
